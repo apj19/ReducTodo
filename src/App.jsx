b6451e5dbadb318ee3031f649abe4e6ffb33c5d8
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-03-15 13:41:55 +0530

    new UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ function App() {
   const notify = () => toast("Wow so easy!");
 
   return (
-    <div
-      className="min-w-[375px] max-w-[1440px] min-h-[100vh] bg-cover bg-center flex justify-center items-start pt-16 overflow-x-hidden"
-      style={{ backgroundImage: "url('./bg.jpg')" }}
-    >
+    <div className="min-w-[375px] max-w-[1440px] min-h-[100vh]  flex flex-col items-center  pt-16 overflow-x-hidden bg-[#181824]">
+      <div
+        className=" w-full h-[50%] absolute top-0 left-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('./bg-desktop-dark.jpg')" }}
+      ></div>
+      <h2 className="z-10 text-white text-[2rem] tracking-[2rem]">TODO</h2>
       <Todo />
       <div>
         <ToastContainer autoClose={500} position="bottom-left" />
